3e0a2ab2c43b7ee4543a92477f2033a3629ca274
Mary Beth <mbingramm@gmail.com>
2021-04-12 11:21:14 -0600

    background tings

--- src/containers/Cult.js
+++ src/containers/Cult.js
@@ -4,9 +4,9 @@ import FollowerCard from '../components/FollowerCard'
 
 export default function Cult() {
     return (
-        <div className="cult-container" style={{ backgroundImage: `url(${background})` }}>
+        <div className="cult-container" style={{ backgroundImage: {background} }}>
+            {background}
             <FollowerCard />
         </div>
     )
 }
-
